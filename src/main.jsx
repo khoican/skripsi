5501d04f7ba61a3468e0e81df7a225405a31cb82
iQ <estrizqy@gmail.com>
2023-12-28 13:01:35 +0700

    update layout home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <Home />,
+// 	}
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+		<BrowserRouter>
+            <Home/>
+		</BrowserRouter>
+    </React.StrictMode>
+);
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navigation from "../../components/Navbar/Navigation";
+
+function Home() {
+    return (
+        <>
+            <div className="">
+                <Navigation/>
+            </div>
+        </>
+
+    )
+}
+
+export default Home
