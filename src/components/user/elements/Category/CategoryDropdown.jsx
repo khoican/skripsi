832215cc76f8f1ac65172b923bb966cc0282c494
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-01 15:12:29 +0700

    style: handling style when dropdown isOpen

--- src/components/user/elements/Category/CategoryDropdown.jsx
+++ src/components/user/elements/Category/CategoryDropdown.jsx
@@ -10,12 +10,12 @@ const CategoryDropdown = (props) => {
 	const { category, children } = props
 
 	const [open, setOpen] = useState(false)
-	const handleClick = () => setOpen(!open)
+	const handleDropdown = () => setOpen(!open)
 
 	return (
 		<Fragment>
-			<div className="px-4 text-slate-700 cursor-pointer hover:bg-primary hover:text-white" onClick={handleClick}>
-				<div className="flex justify-between items-center px-2.5 py-2.5 border-b border-b-slate-300 hover:border-b-primary" >
+			<div className={ `px-4 text-slate-700 cursor-pointer hover:bg-primary hover:text-white ${open && 'bg-primary text-white'}` } onClick={handleDropdown}>
+				<div className={ `flex justify-between items-center px-2.5 py-2.5 border-b border-b-slate-300 hover:border-b-primary ${open && 'bg-primary text-white border-b-primary'}` } >
 					<div className="flex items-center gap-2.5">
 						<ListBulletIcon className="w-5 h-5" />
 						<p>{category}</p>
@@ -33,8 +33,8 @@ const SubCategory = (props) => {
 
 	return (
 		<Fragment>
-			<div className="ps-10 pe-4 text-slate-700 cursor-pointer">
-				<div className="flex items-center gap-2.5 px-2.5 py-2.5 border-b border-b-slate-300">
+			<div className="ps-10 pe-4 text-slate-700 cursor-pointer hover:bg-green hover:text-white">
+				<div className="flex items-center gap-2.5 px-2.5 py-2.5 border-b border-b-slate-300 hover:border-b-green">
 					<HashtagIcon className="w-5 h-5" />
 					<p>{subCategory}</p>
 				</div>
