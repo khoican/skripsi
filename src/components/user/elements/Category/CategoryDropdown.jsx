465b023613e2c3f350c46d4e24e3c12afdcc234c
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-01 14:44:12 +0700

    perf: consuming category api

--- src/components/user/elements/Category/CategoryDropdown.jsx
+++ src/components/user/elements/Category/CategoryDropdown.jsx
@@ -14,8 +14,8 @@ const CategoryDropdown = (props) => {
 
 	return (
 		<Fragment>
-			<div className="px-4 text-slate-700 cursor-pointer" onClick={handleClick}>
-				<div className="flex justify-between items-center px-2.5 py-2.5 border-b border-b-slate-300" >
+			<div className="px-4 text-slate-700 cursor-pointer hover:bg-primary hover:text-white" onClick={handleClick}>
+				<div className="flex justify-between items-center px-2.5 py-2.5 border-b border-b-slate-300 hover:border-b-primary" >
 					<div className="flex items-center gap-2.5">
 						<ListBulletIcon className="w-5 h-5" />
 						<p>{category}</p>
--- src/components/user/elements/Category/Index.jsx
+++ src/components/user/elements/Category/Index.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
-import { getCategory } from '../../../../services/getCategory.js';
+import { getCategory, getSubCategory } from '../../../../services/category.services.js';
 import CategoryDropdown from './CategoryDropdown';
 import { useEffect, useState } from 'react';
 
 const Category = () => {
 	const [categories, setCategories] = useState([]);
+	const [subCategories, setSubCategories] = useState([]);
 
 	useEffect(() => {
 		getCategory((data) => {
@@ -12,19 +13,23 @@ const Category = () => {
 		})
 	}, [])
 
+	useEffect(() => {
+		getSubCategory((data) => {
+			setSubCategories(data)
+		})
+	}, []) 
+
 	return (
 		<div className="w-64">
 			<div className="w-full px-4 py-4 bg-slate-300 rounded-t-md">
 				<h1 className="font-montserrat font-semibold">KATEGORI</h1>
 			</div>
 			{ categories.length > 0 && categories.map((category, index) => (
-				<div key={index}>
-					<CategoryDropdown category={category.categoryName}>
-						{ category.subCategories.map((subCategory, indexes) => (
-							<CategoryDropdown.SubCategory key={indexes} subCategory={subCategory.subCategoryName} />
-						))}
-					</CategoryDropdown>
-				</div>
+				<CategoryDropdown key={index} category={category.name}>
+					{ subCategories.filter((subCategory) => subCategory.category.id === category.id).map((item, index) => (
+						<CategoryDropdown.SubCategory key={index} subCategory={item.name} />
+					))}
+				</CategoryDropdown>
 			))}
 			
 		</div>
