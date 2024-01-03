9467e6d53f804ebe50180c4a3aec7ea9bcd030a0
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-03 11:52:13 +0700

    perf: change method to get product by category

--- src/components/user/fragments/Product/Index.jsx
+++ src/components/user/fragments/Product/Index.jsx
@@ -15,19 +15,7 @@ const Product = () => {
     return (
         <Fragment>
             <main className="flex flex-wrap gap-4 mb-5">
-                {/* { id != null ? product.filter((item) => item.subCategoryId === id).map((item, index) => (
-                    <CardProduct key={index}>
-                        <CardProduct.Header />
-                        <CardProduct.Body name={item.name} price={item.price} />
-                    </CardProduct>
-                )) : 
-                    product.map((item, index) => (
-                    <CardProduct key={index}>
-                        <CardProduct.Header />
-                        <CardProduct.Body name={item.name} price={item.price} />
-                    </CardProduct>
-                ))} */}
-                   { product.map((item, index) => (
+                { product.map((item, index) => (
                     <CardProduct key={index}>
                         <CardProduct.Header />
                         <CardProduct.Body name={item.name} price={item.price} />
