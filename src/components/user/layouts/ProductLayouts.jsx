b71a9ed0502453b3ad2035ee864a7f0305430971
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-03 11:42:35 +0700

    perf: change method to get product by category

--- src/components/user/layouts/ProductLayouts.jsx
+++ src/components/user/layouts/ProductLayouts.jsx
@@ -1,8 +1,19 @@
-import { Fragment } from "react"
+import { Fragment, useState, useEffect } from "react"
 import Category from "./../elements/Category/Index"
 import Product from "../fragments/Product/Index"
+import { useParams } from "react-router-dom"
+import { getProducts } from "../../../services/product.services"
 
 const ProductLayouts = () => {
+    const { id } = useParams()
+    const [ product, setProduct ] = useState([])
+
+    useEffect(() => {
+        getProducts((data) => {
+            setProduct(data)
+        })
+    }, [])
+    
     return (
         <Fragment>
             <main className="w-full flex gap-14 px-28 mt-10">
@@ -10,7 +21,9 @@ const ProductLayouts = () => {
 
                 <main className="w-9/12">
                     <div>
-						<h1 className='text-montserrat font-bold text-xl mb-5'>Menampilkan Semua Produk</h1>
+						<h1 className='text-montserrat font-bold text-xl mb-5'>Menampilkan {' '}
+                        { id != null ? product.filter((item) => item.subCategoryId === id).length : product.length }
+                        {' '} Produk</h1>
 						<Product />
 					</div>
                 </main>
