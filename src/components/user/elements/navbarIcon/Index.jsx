8078eea08efe7c02f25527a1dca6f5680e22035c
khoican <ibnukhoirulprasetyo@gmail.com>
2024-03-20 23:09:05 +0700

    fix: styling link equals current path

--- src/components/user/elements/navbarIcon/Index.jsx
+++ src/components/user/elements/navbarIcon/Index.jsx
@@ -1,16 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, matchPath, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavbarIcon = (props) => {
 	const { link, icon } = props;
-	const currentPath = useLocation().pathname;
+	const currentPath = location.pathname.split('/')[1];
 
 	return (
 		<Link to={link}>
 			<FontAwesomeIcon
 				icon={icon}
 				className={`${
-					currentPath === link ? 'text-green-700' : 'text-gray-400'
+					'/' + currentPath === link
+						? 'text-green-700'
+						: 'text-gray-400'
 				} hover:text-green-700 w-7 h-7`}
 			/>
 		</Link>
