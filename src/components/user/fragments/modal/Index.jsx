3ba8f592975fd656c991b4cf1c79a3f9489f0afc
khoican <88487817+khoican@users.noreply.github.com>
2024-07-31 21:59:15 +0700

    feat: checkout responsive

--- src/components/user/fragments/modal/Index.jsx
+++ src/components/user/fragments/modal/Index.jsx
@@ -4,10 +4,10 @@ const Modal = (props) => {
 	const { onDelete, onClose, children, title, onSave } = props;
 
 	return (
-		<div className="fixed inset-0 z-20 bg-black bg-opacity-50 flex items-center justify-center">
-			<div className="bg-white w-2/4 rounded-md ">
+		<div className="fixed inset-0 z-20 bg-black bg-opacity-50 flex items-center justify-center px-5">
+			<div className="bg-white w-full md:w-2/4 rounded-md ">
 				<div>
-					<h1 className="text-lg font-semibold pt-5 px-5 pb-2 border-b border-gray-500 uppercase">
+					<h1 className="text-sm md:text-lg font-semibold pt-5 px-5 pb-2 border-b border-gray-500 uppercase">
 						{title}
 					</h1>
 				</div>
@@ -26,7 +26,9 @@ const Modal = (props) => {
 							<Button
 								text={'Hapus'}
 								onClick={onDelete}
-								style={'bg-danger text-white'}
+								style={
+									'bg-danger text-white text-xs md:text-base'
+								}
 							/>
 						)}
 
@@ -34,7 +36,9 @@ const Modal = (props) => {
 							<Button
 								text={'Lanjutkan'}
 								onClick={onSave}
-								style={'bg-success text-white'}
+								style={
+									'bg-success text-white text-xs md:text-base'
+								}
 							/>
 						)}
 					</div>
